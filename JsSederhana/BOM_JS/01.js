48c7b63f166d3dab0633f77c8cc561b0e7cb94ee
RasyaPrtma <rasyaputraq17@gmail.com>
2024-07-23 09:40:04 +0700

    add feature

---
+++ JsSederhana/BOM_JS/01.js
@@ -0,0 +1,11 @@
+// window.open('#')
+// For Open New Window
+
+// window.close()
+// For Close Current Window
+
+const text = document.querySelector("h1");
+
+text.addEventListener("click", function () {
+  window.resizeTo(200,200)
+});
